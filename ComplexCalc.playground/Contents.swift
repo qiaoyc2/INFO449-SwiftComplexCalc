0fc6feb5e0b5b142cdaad55f3437d5fe5c5307ca
Katharina Cheng <qiaoyc2@uw.edu>
2026-01-20 21:23:39 -0800

    mathOp works

--- ComplexCalc.playground/Contents.swift
+++ ComplexCalc.playground/Contents.swift
@@ -44,6 +44,11 @@ class Calculator {
     func divide(lhs: Int, rhs: Int) -> Int {
         lhs / rhs
     }
+    
+    func mathOp(lhs: Int, rhs: Int, op: (Int, Int) -> Int) -> Int {
+        op(lhs, rhs)
+    }
+    
 }
 
 //: Don't change the name of this object (`calc`); it's used in all the tests.
@@ -68,11 +73,11 @@ calc.subtract(lhs: 2, rhs: 2) == 0
 calc.multiply(lhs: 2, rhs: 2) == 4
 calc.divide(lhs: 2, rhs: 2) == 1
 
-//calc.mathOp(lhs: 5, rhs: 5, op: { (lhs: Int, rhs: Int) -> Int in (lhs + rhs) + (lhs * rhs) }) == 35
-//    // This style is one way of writing an anonymous function
-//calc.mathOp(lhs: 10, rhs: -5, op: { ($0 + $1) + ($0 - $1) }) == 20
-//    // This is the second, more terse, style; either works
-//
+calc.mathOp(lhs: 5, rhs: 5, op: { (lhs: Int, rhs: Int) -> Int in (lhs + rhs) + (lhs * rhs) }) == 35
+    // This style is one way of writing an anonymous function
+calc.mathOp(lhs: 10, rhs: -5, op: { ($0 + $1) + ($0 - $1) }) == 20
+    // This is the second, more terse, style; either works
+
 //calc.add([1, 2, 3, 4, 5]) == 15
 //calc.multiply([1, 2, 3, 4, 5]) == 120
 //calc.count([1, 2, 3, 4, 5, 6, 7, 8]) == 8
